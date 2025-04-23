380838b23ba37b8b1cc4c39637cb7579b5d27b0a
Shuhrat Asrorov <asrorovshuhratbek@gmail.com>
2025-04-23 13:17:43 +0300

    Создание тестового бина с помощью applicationContext.xml

---
+++ SpringTestApp/src/main/java/dev/SpringTest.java
@@ -0,0 +1,16 @@
+package dev;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
