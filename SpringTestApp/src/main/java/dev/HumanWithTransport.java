b0c0c1dc2ab5e23f376ded626627383832c3c0e3
Shuhrat Asrorov <asrorovshuhratbek@gmail.com>
2025-05-02 23:40:18 +0300

    Научился внедрять зависимости с помощью аннотаций даже если несколько несколько бинов подходят под одну задачу

--- SpringTestApp/src/main/java/dev/HumanWithTransport.java
+++ SpringTestApp/src/main/java/dev/HumanWithTransport.java
@@ -1,29 +1,19 @@
 package dev;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HumanWithTransport{
-    private Car car;
-    private Moto moto;
-    @Autowired
-    private Bike bike;
+    private Transport transport;
 
     @Autowired
-    public void setMoto(Moto moto) {
-        this.moto = moto;
-    }
-
-
-    @Autowired
-    public HumanWithTransport(Car car){
-        this.car = car;
+    public HumanWithTransport(@Qualifier("car")Transport transport){
+        this.transport = transport;
     }
 
     public void moveHuman(){
-        System.out.println("I " + bike.move());
-        System.out.println("I " + car.move());
-        System.out.println("I " + moto.move());
+        System.out.println("I " + transport.move());
     }
 }
